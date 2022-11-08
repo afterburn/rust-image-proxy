e98c921e23d39256602aaeb23d4adc9918196d58
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2022-11-08 21:20:05 +0100

    feat: initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,152 +1,98 @@
-use anyhow::Result;
-use axum::{
-    body::{self, Bytes},
-    extract::Query,
-    http::{HeaderValue, Response, StatusCode},
-    response::{Html, IntoResponse},
-    routing::get,
-    Json, Router,
+use actix_web::{
+    get,
+    web::{self, Bytes},
+    App, HttpRequest, HttpResponse, HttpServer,
 };
-use http_body::{Empty, Full};
-use image::EncodableLayout;
-use include_dir::{include_dir, Dir};
-use reqwest::header;
-use serde::{de, Deserialize, Deserializer};
-use serde_json::{json, Value};
-use std::{fmt, fs::File, io::Write, str::FromStr};
-use urlencoding::decode;
-use webp::{Encoder, WebPMemory};
-static TMP_DIR: Dir<'_> = include_dir!("./tmp");
-
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
-
-async fn json() -> (StatusCode, Json<Value>) {
-    (StatusCode::BAD_REQUEST, Json(json!({ "data": 42 })))
-}
-
-async fn html() -> (StatusCode, Html<&'static str>) {
-    (StatusCode::OK, Html("<h1>Hello, World!</h1>"))
-}
+use image::{imageops::FilterType, DynamicImage, EncodableLayout};
+use serde::Deserialize;
+use std::{fs::File, io::Write};
+use webp::Encoder;
 
 #[derive(Debug, Deserialize)]
-#[allow(dead_code)]
-struct ImageRequestParams {
-    #[serde(default, deserialize_with = "empty_string_as_none")]
-    url: Option<String>,
+pub struct ImageRequest {
+    url: String,
+    w: Option<u32>,
+    h: Option<u32>,
 }
 
-fn empty_string_as_none<'de, D, T>(de: D) -> Result<Option<T>, D::Error>
-where
-    D: Deserializer<'de>,
-    T: FromStr,
-    T::Err: fmt::Display,
-{
-    let opt = Option::<String>::deserialize(de)?;
-    match opt.as_deref() {
-        None | Some("") => Ok(None),
-        Some(s) => FromStr::from_str(s).map_err(de::Error::custom).map(Some),
+#[get("/")]
+async fn proxy_image(req: HttpRequest, params: web::Query<ImageRequest>) -> HttpResponse {
+    // Generate id
+    let id = "abc123".to_owned();
+
+    // Download image
+    let bytes = match get_image_bytes(params.url.clone()).await {
+        Ok(v) => v,
+        Err(_) => todo!(),
+    };
+
+    // Load image into memory.
+    let mut img = image::load_from_memory(&bytes).unwrap();
+
+    // Resize
+    if params.w.is_some() || params.h.is_some() {
+        let original_width = img.width() as f32;
+        let original_height = img.height() as f32;
+
+        let width: u32;
+        let height: u32;
+
+        if params.w.is_some() && params.h.is_none() {
+            let aspect_ratio = original_height / original_width;
+            width = params.w.unwrap();
+            height = (width as f32 * aspect_ratio).ceil() as u32;
+        } else if params.w.is_none() && params.h.is_some() {
+            let aspect_ratio = original_width / original_height;
+            height = params.h.unwrap();
+            width = (height as f32 * aspect_ratio).ceil() as u32;
+        } else {
+            width = params.w.unwrap();
+            height = params.h.unwrap();
+        }
+
+        img = DynamicImage::resize_exact(&img, width, height, FilterType::Gaussian);
     }
+
+    // Convert to webp
+    let webp_path = image_to_webp(img, format!("./tmp/{}.webp", id)).unwrap();
+
+    // Serve webp
+    let file = actix_files::NamedFile::open_async(webp_path).await.unwrap();
+    file.into_response(&req)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    match create_dir("./tmp".to_owned()) {
+        Ok(_) => {}
+        Err(_) => panic!("Could not create tmp directory."),
+    };
+
+    HttpServer::new(|| App::new().service(proxy_image))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
 
 // Load image from url and place in memory.
-async fn get_image_bytes(url: String) -> Result<Bytes> {
+async fn get_image_bytes(url: String) -> anyhow::Result<Bytes> {
     let bytes = reqwest::get(url).await?.bytes().await?;
-
     return Ok(bytes);
 }
 
-fn create_dir(dir_path: String) -> Result<()> {
-    std::fs::create_dir_all(dir_path)?;
-    Ok(())
-}
+// Convert bytes into webp format.
+fn image_to_webp(img: DynamicImage, webp_path: String) -> Result<String, std::io::Error> {
+    let encoder = Encoder::from_image(&img).unwrap();
+    let encoded_webp = encoder.encode(65f32);
 
-fn convert_bytes_into_webp(id: String, bytes: Bytes) -> Result<String, std::io::Error> {
-    let img = image::load_from_memory(&bytes).unwrap();
-    let encoder: Encoder = Encoder::from_image(&img).unwrap();
-    let encoded_webp: WebPMemory = encoder.encode(65f32);
-
-    let webp_image_path = format!("./tmp/{}.webp", id);
-    let mut webp_image = File::create(webp_image_path.to_string()).unwrap();
+    let mut webp_image = File::create(webp_path.to_string()).unwrap();
     match webp_image.write_all(encoded_webp.as_bytes()) {
-        Ok(_) => Ok(webp_image_path),
+        Ok(_) => Ok(webp_path),
         Err(err) => Err(err),
     }
 }
 
-async fn download_and_convert_image(id: String, image_url: String) -> Result<String> {
-    let image_url_decoded = match decode(&image_url) {
-        Ok(it) => it.to_string(),
-        Err(_) => {
-            todo!()
-        }
-    };
-
-    let bytes = get_image_bytes(image_url_decoded).await?;
-    Ok(convert_bytes_into_webp(id, bytes)?)
-}
-
-async fn proxy_handler(params: Query<ImageRequestParams>) -> impl IntoResponse {
-    let params: ImageRequestParams = params.0;
-
-    let image_url = match params.url {
-        Some(url) => url,
-        None => "".to_owned(),
-    };
-
-    if image_url.is_empty() {
-        (
-            StatusCode::BAD_REQUEST,
-            Json(json!({ "error": "Bad request" })),
-        );
-    }
-
-    let id = "test".to_owned();
-
-    let webp_path = match download_and_convert_image(id, image_url).await {
-        Err(_) => "".to_owned(),
-        Ok(webp_path) => webp_path,
-    };
-
-    // if webp_path.is_empty() {
-    //     return Err((
-    //         StatusCode::INTERNAL_SERVER_ERROR,
-    //         Json(json!({ "error": "Internal server error" })),
-    //     ));
-    // };
-
-    match TMP_DIR.get_file("test.webp") {
-        None => Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .body(body::boxed(Empty::new()))
-            .unwrap(),
-        Some(file) => Response::builder()
-            .status(StatusCode::OK)
-            .header(
-                header::CONTENT_TYPE,
-                HeaderValue::from_str("webp".as_ref()).unwrap(),
-            )
-            .body(body::boxed(Full::from(file.contents())))
-            .unwrap(),
-    }
-}
-
-#[tokio::main]
-async fn main() {
-    match create_dir("./tmp".to_owned()) {
-        Ok(_) => {}
-        Err(_) => panic!("Could not create tmp directory."),
-    }
-
-    // build our application with a single route
-    let app = Router::new()
-        .route("/", get(html))
-        .route("/proxy", get(proxy_handler));
-
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+fn create_dir(dir_path: String) -> anyhow::Result<()> {
+    std::fs::create_dir_all(dir_path)?;
+    Ok(())
 }
